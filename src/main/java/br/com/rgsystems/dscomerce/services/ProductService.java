8e5a1bca56a26ee8b1d06b9caef2e24c2d68a157
Rafael Gomes <gorraffa@hotmail.com>
2025-05-04 10:43:32 -0300

    delete produtc

--- src/main/java/br/com/rgsystems/dscomerce/services/ProductService.java
+++ src/main/java/br/com/rgsystems/dscomerce/services/ProductService.java
@@ -47,10 +47,16 @@ public class ProductService {
         return new ProductDTO(entity);
     }
 
+    @Transactional()
+    public void delete(Long id){
+        repository.deleteById(id);
+    }
+
     private void copyDTOToEntity(ProductDTO dto, Product entity) {
         entity.setName(dto.getName());
         entity.setDescription(dto.getDescription());
         entity.setImgUrl(dto.getImgUrl());
         entity.setPrice(dto.getPrice());
     }
+
 }
